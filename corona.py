b1d302a04981f8634d47c45e30b815b1dd236dc6
anonkyuhuu <57594865+anonkyuhuu@users.noreply.github.com>
2020-03-27 00:08:10 +0700

    Update corona.py

--- corona.py
+++ corona.py
@@ -20,7 +20,7 @@ class Corona:
   self.b = requests.get(self.indo).json()
   for x in self.b:
    runtext.load('\x1b[37m[+] Positive : '+x['positif'])
-   runtext.load('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovery : '+x['sembuh'])
+   runtext.load('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovered : '+x['sembuh'])
    runtext.load('\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : '+x['meninggal'])
    time.sleep(3)
   self.ask = raw_input('\x1b[37mShow Province (y/n) ')
@@ -33,7 +33,7 @@ class Corona:
     print('═'*25)
     print('\x1b[37m[~] Province : '+pv)
     print('\x1b[37m[+] Positive : '+str(a['Kasus_Posi']))
-    print('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovery : '+str(a['Kasus_Semb']))
+    print('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovered : '+str(a['Kasus_Semb']))
     print('\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : '+str(a['Kasus_Meni'])+'\x1b[37m')
   if 'n' in self.ask:
    exit()
@@ -48,7 +48,7 @@ class Corona:
    self.xx = re.search(self.t,self.p)
    print('\n{} Info'.format(self.wrl))
    print('''\x1b[37m[+] Positive : {}
-\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovery : {}
+\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovered : {}
 \x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : {}\x1b[37m'''.format(self.xx.group(1),self.xx.group(2),self.xx.group(3)))
 
  def main(self):
