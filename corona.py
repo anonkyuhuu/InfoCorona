fb396441f8ebf07dae1af521e41f071497c73f40
anonkyuhuu <57594865+anonkyuhuu@users.noreply.github.com>
2020-03-27 00:06:25 +0700

    Update corona.py

--- corona.py
+++ corona.py
@@ -6,61 +6,64 @@ Versi  : 0.3 (Latest Update)
 
 Jangan recode ya kontol codingan gue ga rapih
 '''
+class Corona:
 
-import requests,os,runtext,sys,time,re
-from difflib import get_close_matches as src
+ def __init__(self):
+  self.prov = 'https://api.kawalcorona.com/indonesia/provinsi'
+  self.indo = 'https://api.kawalcorona.com/indonesia/'
+  self.coun = 'https://api.kawalcorona.com/'
+  self.main()
 
-os.system('clear')
-url = 'https://{}'
+ def indonesia(self):
+  self.res = requests.get(self.prov).json()
+  time.sleep(5)
+  self.b = requests.get(self.indo).json()
+  for x in self.b:
+   runtext.load('\x1b[37m[+] Positive : '+x['positif'])
+   runtext.load('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovery : '+x['sembuh'])
+   runtext.load('\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : '+x['meninggal'])
+   time.sleep(3)
+  self.ask = raw_input('\x1b[37mShow Province (y/n) ')
+  if 'y' in self.ask:
+   n = []
+   for i in self.res:
+    n.append(i['attributes'])
+   for a in n:
+    pv = a['Provinsi']
+    print('═'*25)
+    print('\x1b[37m[~] Province : '+pv)
+    print('\x1b[37m[+] Positive : '+str(a['Kasus_Posi']))
+    print('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovery : '+str(a['Kasus_Semb']))
+    print('\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : '+str(a['Kasus_Meni'])+'\x1b[37m')
+  if 'n' in self.ask:
+   exit()
 
-def indonesia():
- res = requests.get(url.format('api.kawalcorona.com/indonesia/provinsi')).json()
- time.sleep(5)
- b = requests.get(url.format('api.kawalcorona.com/indonesia')).json()
- for x in b:
-  runtext.load('\x1b[37m[+] Positive : '+x['positif'])
-  runtext.load('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recover : '+x['sembuh'])
-  runtext.load('\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : '+x['meninggal'])
-  time.sleep(3)
- ask = raw_input('\x1b[37mTampilkan Wilayah (y/n) ')
- if 'y' in ask:
-  n = []
-  for i in res:
-   n.append(i['attributes'])
-  for a in n:
-   pv = a['Provinsi']
-   print('\x1b[37m[~] Province : '+pv)
-   print('\x1b[37m[+] Positive : '+str(a['Kasus_Posi']))
-   print('\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recover : '+str(a['Kasus_Semb']))
-   print('\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : '+str(a['Kasus_Meni'])+'\x1b[37m')
- if 'n' in ask:
-  exit()
+ def all_country(self):
+  self.p = requests.get(self.coun).text
+  self.cnt = re.findall('"Country_Region":"(.*?)"',self.p)
+  while True:
+   self.mm = raw_input('Country : ')
+   self.wrl = "".join(src(self.mm,self.cnt,n=1,cutoff=0))
+   self.t= r'{"OBJECTID":.*?,"Country_Region":"'+self.wrl+'","Last_Update":.*?,"Lat":.*?,"Long_":.*?,"Confirmed":(.*?),"Deaths":(.*?),"Recovered":(.*?),"Active":.*?}}'
+   self.xx = re.search(self.t,self.p)
+   print('\n{} Info'.format(self.wrl))
+   print('''\x1b[37m[+] Positive : {}
+\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recovery : {}
+\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : {}\x1b[37m'''.format(self.xx.group(1),self.xx.group(2),self.xx.group(3)))
 
-def all_country():
- p = requests.get(url.format('api.kawalcorona.com')).text
- cnt = re.findall('"Country_Region":"(.*?)"',p)
- while True:
-  mm = raw_input('Negara : ')
-  wrl = "".join(src(mm,cnt,n=1,cutoff=0))
-  t= r'{"OBJECTID":.*?,"Country_Region":"'+wrl+'","Last_Update":.*?,"Lat":.*?,"Long_":.*?,"Confirmed":(.*?),"Deaths":(.*?),"Recovered":(.*?),"Active":.*?}}'
-  xx = re.search(t,p)
-  print('\n{} Info'.format(wrl))
-  print('''\x1b[37m[+] Positive : {}
-\x1b[37m[\x1b[32;1m*\x1b[37m]\x1b[32;1m Recover : {}
-\x1b[37m[\x1b[31m-\x1b[37m]\x1b[31m Died : {}\x1b[37m'''.format(xx.group(1),xx.group(2),xx.group(3)))
-
-def main():
- print('''1. Indonesian Status
+ def main(self):
+  print('''1. Indonesian Status
 2. All Country Status
 ''')
- while True:
-  nanya = int(raw_input('Country >> '))
+  nanya = int(raw_input('status >> '))
   if nanya == 1:
-   indonesia()
+   self.indonesia()
   if nanya == 2:
-   all_country()
+   self.all_country()
 
-if __name__ == '__main__':
+try:
+ import requests,os,runtext,sys,time,re
+ from difflib import get_close_matches as src
  os.system('clear')
  print('''\x1b[32;1m ▄▄·       ▄▄▄         ▐ ▄  ▄▄▄·
 ▐█ ▌▪▪     ▀▄ █·▪     •█▌▐█▐█ ▀█
@@ -68,4 +71,6 @@ if __name__ == '__main__':
 ▐███▌▐█▌.▐▌▐█•█▌▐█▌.▐▌██▐█▌▐█ ▪▐▌
  ▀▀▀  ▀█▄▀▪.▀  ▀ ▀█▄▀▪▀▀ █▪ ▀  ▀ \x1b[37mv0.3
 ''')
- main()
+ Corona()
+except requests.exceptions.ConnectionError:
+ exit('No Connection')
